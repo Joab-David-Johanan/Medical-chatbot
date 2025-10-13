62ee929708bb9d6625c175a0e31ac4cb0a0b996f
Joab David Johanan <joabkindle@gmail.com>
2025-10-14 01:00:17 +0200

    added folder structure

---
+++ template.sh
@@ -0,0 +1,17 @@
+# creating directories
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/trail.ipynb
+touch .env
+touch app.py
+touch setup.py
+touch requirements.txt
+
+echo "Directory and files created successfully!"
+
+
